c3a1f0a091f3bbb6de51d29076b21f13745f84cd
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-03-28 22:14:08 -0300

    tratando erro 400

--- src/main/java/conn/cursos/infra/TratadorDeErros.java
+++ src/main/java/conn/cursos/infra/TratadorDeErros.java
@@ -2,13 +2,28 @@ package conn.cursos.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<?> error404() {
         return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<?>> error400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErros::new).toList());
+}
+    public record DadosErros(String field, String message) {
+        public DadosErros(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+
     }
 }
