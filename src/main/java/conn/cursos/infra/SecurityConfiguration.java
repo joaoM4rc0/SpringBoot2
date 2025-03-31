e7c7d375734714d4be5a96e955c4129ef32f0f77
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-03-31 15:54:15 -0300

    Classe de configuração, essa é configuração minima, que só faz tirar a tela de segurança(tela de login, mostrada mais acima) do spring, e isso não é um problema pois vamos configura-la nos mesmos.

---
+++ src/main/java/conn/cursos/infra/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package conn.cursos.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(Customizer.withDefaults())
+                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
