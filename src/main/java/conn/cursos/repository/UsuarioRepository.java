5352ff5d7017bab46d77df3cc74850cb044fd1a3
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-03-31 14:59:33 -0300

    Service de Autenticação

--- src/main/java/conn/cursos/repository/UsuarioRepository.java
+++ src/main/java/conn/cursos/repository/UsuarioRepository.java
@@ -3,9 +3,11 @@ package conn.cursos.repository;
 import conn.cursos.remedio.Remedio;
 import conn.cursos.usuarios.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.List;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
+    UserDetails findByLogin(String login);
 }
