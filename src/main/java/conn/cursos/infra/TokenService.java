abb6cd700760428600928b85ed0804e8eea15d58
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-04-02 16:00:38 -0300

    melhorando a forma de retorno do token

--- src/main/java/conn/cursos/infra/TokenService.java
+++ src/main/java/conn/cursos/infra/TokenService.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import conn.cursos.usuarios.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -11,10 +12,13 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 @Service
 public class TokenService {
+    @Value("${api:security:token:secret}")
+    private String secret;
+
     public String gerarToken(Usuario usuario) {
         try {
             // Cria algoritmo de assinatura usando HMAC256 com chave "123456"
-            var algorithm = Algorithm.HMAC256("123456");
+            var algorithm = Algorithm.HMAC256(secret);
 
             // Constrói o token JWT
             return JWT.create()
