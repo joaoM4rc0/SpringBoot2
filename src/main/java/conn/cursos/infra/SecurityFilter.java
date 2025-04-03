619f72dd55cc654fd9793d50e2f52e3db0340ce1
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2025-04-03 13:59:46 -0300

    fazendo a verificação do token

--- src/main/java/conn/cursos/infra/SecurityFilter.java
+++ src/main/java/conn/cursos/infra/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,8 +12,18 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenJWT = recuperaToken(request);
+        String subject = tokenService.getSubject(tokenJWT);
         filterChain.doFilter(request, response);
+    }
+
+    private String recuperaToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader == null ) throw new RuntimeException("token nao enviado");
+        return authorizationHeader;
     }
 }
--- src/main/java/conn/cursos/infra/TokenService.java
+++ src/main/java/conn/cursos/infra/TokenService.java
@@ -3,6 +3,7 @@ package conn.cursos.infra;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import conn.cursos.usuarios.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,7 +31,19 @@ public class TokenService {
             throw new RuntimeException("erro ao gerar token, erro: ", e);
         }
     }
+    public String getSubject(String tokenJWT) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("remedios_api")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
 
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("token invalido ou expirado");
+        }
+    }
     private Instant DataExpiracao() {
         // Calcula data de expiração: 2 horas a partir de agora
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.UTC);
